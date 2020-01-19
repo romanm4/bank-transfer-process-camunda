c38e0d5ac402770900107a9678848c25e06d7fc2
Roman <romanmaniev@gmail.com>
2020-01-19 23:34:17 +0200

    delete json converter class

--- src/main/java/com/bank/transfer/mapper/IJsonConverter.java
+++
@@ -1,9 +0,0 @@
-package com.bank.transfer.mapper;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import java.util.Map;
-
-public interface IJsonConverter {
-    Object getConvertJsonToObject(String json) throws JsonProcessingException;
-}

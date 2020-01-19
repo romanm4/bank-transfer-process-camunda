bb54a3a7fb3efd006912707488b71865df288f4b
Roman <romanmaniev@gmail.com>
2020-01-19 23:40:31 +0200

    add CheckClientFoundsFacade class

--- src/main/java/com/bank/transfer/delegate/CheckClientFoundsDelegate.java
+++ src/main/java/com/bank/transfer/delegate/CheckClientFoundsDelegate.java
@@ -1,35 +1,20 @@
 package com.bank.transfer.delegate;
 
-import com.bank.transfer.listener.model.ClientFoundsResponse;
+import com.bank.transfer.facade.CheckClientFoundsFacade;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+
 
 @Component
 public class CheckClientFoundsDelegate implements JavaDelegate {
 
     @Autowired
-    private RestTemplate restTemplate;
-
-    @Autowired
-    private HttpEntity<String> httpEntity;
-
-    @Value("${mock.found.client.url}")
-    private String url;
+    private CheckClientFoundsFacade checkClientFoundsFacade;
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        delegateExecution.setVariable("isLimit", true);
-        delegateExecution.setVariable("isInsufficient", true);
-        delegateExecution.setVariable("isRecipientExist", true);
-        ResponseEntity<ClientFoundsResponse> responseResponseEntity =
-                restTemplate.exchange(url, HttpMethod.GET, httpEntity, ClientFoundsResponse.class);
-        System.out.println(responseResponseEntity.getBody().toString());
+        checkClientFoundsFacade.executeCheckClientFounds(delegateExecution);
     }
 }
---
+++ src/main/java/com/bank/transfer/facade/CheckClientFoundsFacade.java
@@ -0,0 +1,33 @@
+package com.bank.transfer.facade;
+
+import com.bank.transfer.listener.model.ClientFoundsResponse;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class CheckClientFoundsFacade {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private HttpEntity<String> httpEntity;
+
+    @Value("${mock.found.client.url}")
+    private String url;
+
+    public void executeCheckClientFounds(DelegateExecution delegateExecution) {
+        delegateExecution.setVariable("isLimit", true);
+        delegateExecution.setVariable("isInsufficient", true);
+        delegateExecution.setVariable("isRecipientExist", true);
+        ResponseEntity<ClientFoundsResponse> responseResponseEntity =
+                restTemplate.exchange(url, HttpMethod.GET, httpEntity, ClientFoundsResponse.class);
+    }
+
+}
